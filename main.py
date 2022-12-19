578b004427c3f91beafc17ae87010c3b5de4927d
fedyann <annfedotova6@yandex.ru>
2022-12-19 19:45:45 +0300

    creating main file

---
+++ main.py
@@ -0,0 +1,47 @@
+import os, sys, math, pygame, pygame.mixer
+from pygame.locals import *
+
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
+run_me = True
+
+screen_size = screen_width, screen_height = 600, 400
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('ha ha ha ha ha')
+
+clock = pygame.time.Clock()
+fps_limit = 60
+
+# circle
+colorcircle = (red)
+posx = 300
+posy = 200
+circle = pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
+
+while run_me:
+    clock.tick(fps_limit)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run_me = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                posx = posx - 10
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                posx = posx + 10
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                posy = posy + 10
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                posy = posy - 10
+    screen.fill(black)
+    pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
+
+    pygame.display.flip()
+
+pygame.quit()
