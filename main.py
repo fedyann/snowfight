ccb61751805d7ca90e51d0dbb113b6017d96cbee
fedyann <annfedotova6@yandex.ru>
2022-12-19 22:35:24 +0300

    creating main file

--- main.py
+++ main.py
@@ -1,47 +1,85 @@
-import os, sys, math, pygame, pygame.mixer
-from pygame.locals import *
+import os
+import sys
 
-black = (0, 0, 0)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-white = (255, 255, 255)
-run_me = True
+import pygame
 
-screen_size = screen_width, screen_height = 600, 400
-screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption('ha ha ha ha ha')
+WIDTH = 1000
+HEIGHT = 700
+FPS = 60
 
-clock = pygame.time.Clock()
-fps_limit = 60
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 
-# circle
-colorcircle = (red)
-posx = 300
-posy = 200
-circle = pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
 
-while run_me:
-    clock.tick(fps_limit)
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    image1 = pygame.transform.scale(image, (100, 100))
+    return image1
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run_me = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                posx = posx - 10
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                posx = posx + 10
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                posy = posy + 10
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                posy = posy - 10
-    screen.fill(black)
-    pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
 
-    pygame.display.flip()
+class Player(pygame.sprite.Sprite):
+    image = load_image("creature.png")
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = Player.image
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.x = 0
+        self.y = 0
+
+    def update(self):
+        self.x = 0
+        self.y = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.x = -8
+        if keystate[pygame.K_RIGHT]:
+            self.x = 8
+        if keystate[pygame.K_UP]:
+            self.y = -8
+        if keystate[pygame.K_DOWN]:
+            self.y = 8
+        self.rect.x += self.x
+        self.rect.y += self.y
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.mixer.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("SnowFight")
+    clock = pygame.time.Clock()
+
+    all_sprites = pygame.sprite.Group()
+    player = Player()
+    all_sprites.add(player)
+    running = True
+    while running:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        all_sprites.update()
+        screen.fill(WHITE)
+        all_sprites.draw(screen)
+        pygame.display.flip()
 
 pygame.quit()
