b5516913a5432199b83c35172944246f4618b48e
fedyann <annfedotova6@yandex.ru>
2022-12-26 23:38:06 +0300

    creating level

--- main.py
+++ main.py
@@ -10,50 +10,76 @@ FPS = 60
 WHITE = (255, 255, 255)
 
 
-def load_image(name, colorkey=None):
+def load_image(name, size, colorkey=None):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
-    image1 = pygame.transform.scale(image, (100, 100))
+    image1 = pygame.transform.scale(image, size)
     return image1
 
 
 class Player(pygame.sprite.Sprite):
-    image = load_image("creature.png")
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = Player.image
+    def __init__(self, x, y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * x + 15, tile_height * y + 5)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
-        self.x = 0
-        self.y = 0
+        self.speed_x = 0
+        self.speed_y = 0
 
     def update(self):
-        self.x = 0
-        self.y = 0
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.x = -8
-        if keystate[pygame.K_RIGHT]:
-            self.x = 8
-        if keystate[pygame.K_UP]:
-            self.y = -8
-        if keystate[pygame.K_DOWN]:
-            self.y = 8
-        self.rect.x += self.x
-        self.rect.y += self.y
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.top < 0:
-            self.rect.top = 0
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
+        keys = pygame.key.get_pressed()
+        self.speed_x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+        self.speed_y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
+        old_rect = self.rect
+        self.rect = self.rect.move(self.speed_x, self.speed_y)
+        collide_object = pygame.sprite.spritecollide(self, tiles_group, False)
+        if collide_object:
+            self.rect = old_rect
+
+
+def load_level(filename):
+    filename = "levels/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+tile_images = {
+    'wall': load_image('wall.jpg', (50, 50))
+}
+player_image = load_image('santa.gif', (50, 50))
+tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                new_player = Player(x, y)
+    return new_player, x, y
 
 
 if __name__ == '__main__':
@@ -62,10 +88,7 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("SnowFight")
     clock = pygame.time.Clock()
-
-    all_sprites = pygame.sprite.Group()
-    player = Player()
-    all_sprites.add(player)
+    player, level_x, level_y = generate_level(load_level('first'))
     running = True
     while running:
         clock.tick(FPS)
